b47e07da49bfc071abcb6836a901794212558251
lunamoonmoon <5862lunamoon@gmail.com>
2023-08-10 11:30:43 -0700

    add on hover style changes

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $(document).ready(function() { //runs callback when DOM ready and browser loaded
 
-let textInput = document.getElementById("tweet-text"); //get text input by id
+  let textInput = document.getElementById("tweet-text"); //get text input by id
 
-$(textInput).on("input", function() { //event on input in text area
-  let currentCount = this.value.length;
-  let charCount = this.parentElement.querySelector(".counter");
-  
-  charCount.textContent = 140 - currentCount; //char limit 140 - char in text input area
-  if (currentCount> 140) { //if exceed char limit
-    charCount.classList.add("over-count"); //change colour to red
-  } else {
-    charCount.classList.remove("over-count"); //change colour back
-  };
-});
+  $(textInput).on("input", function() { //event on input in text area
+    let currentCount = this.value.length;
+    let charCount = this.form.querySelector(".counter");
+    
+    charCount.textContent = 140 - currentCount; //char limit 140 - char in text input area
+    if (currentCount> 140) { //if exceed char limit
+      charCount.classList.add("over-count"); //change colour to red
+    } else {
+      charCount.classList.remove("over-count"); //change colour back
+    };
+  });
 });
